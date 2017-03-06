bac5dd23e50e47552c01ba6cf9669cb98a0eb8b7
Franz Josef Kaiser <fjk@strg.at>
2017-03-06 23:48:30 +0100

    Feat: Add int examples

--- app/main.go
+++ app/main.go
@@ -16,4 +16,7 @@ func main() {
 
     yours := ! true
     fmt.Println("The world is mine! True or false? ", yours)
+
+    var MC, ML, XXX = 1, 9, 80
+    fmt.Println("Since when is the world yours?", MC, ML, XXX)
 }
